323811ee7142ac99824c2ea3a441b1e8694fe94f
yang1015 <itsak1992@gmail.com>
2019-09-17 09:45:26 +0800

    add better-scroll & alphabet component

--- vue/src/pages/City/City.vue
+++ vue/src/pages/City/City.vue
@@ -2,24 +2,45 @@
   <div>
     <city-header></city-header>
     <city-search-box></city-search-box>
+    <city-list :cities = 'cities' :hotCities = 'hotCities'></city-list>
+    <city-alphabet></city-alphabet>
   </div>
 </template>
 
 <script>
+  import axios from 'axios';
   import CityHeader from './components/Header.vue'
   import CitySearchBox from './components/Search.vue'
+  import CityList from './components/List.vue'
+  import CityAlphabet from './components/Alphabet.vue'
 
   export default {
     name: 'City',
     components: {
       CityHeader,
-      CitySearchBox
+      CitySearchBox,
+      CityList,
+      CityAlphabet
     },
-
+    mounted() {
+      axios.get('/api/city.json')
+        .then(this.getCities)
+        .catch(e => {
+          console.log(e)
+        })
+    },
     data() {
       return {
-
+        cities: [],
+        hotCities: []
       }
+    },
+    methods: {
+      getCities(res) {
+        this.hotCities = res.data.data.hotCities;
+        this.cities = res.data.data.cities;
+
+      },
     }
   }
 </script>
---
+++ vue/src/pages/City/components/Alphabet.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="alphabet-list">
+    <ul>
+      <li class="al-item">A</li>
+      <li class="al-item">B</li>
+      <li class="al-item">C</li>
+      <li class="al-item">D</li>
+      <li class="al-item">E</li>
+      <li class="al-item">F</li>
+      <li class="al-item">G</li>
+      <li class="al-item">H</li>
+      <li class="al-item">J</li>
+      <li class="al-item">K</li>
+      <li class="al-item">L</li>
+      <li class="al-item">M</li>
+      <li class="al-item">N</li>
+      <li class="al-item">P</li>
+      <li class="al-item">Q</li>
+      <li class="al-item">R</li>
+      <li class="al-item">S</li>
+      <li class="al-item">T</li>
+      <li class="al-item">W</li>
+      <li class="al-item">X</li>
+      <li class="al-item">Y</li>
+      <li class="al-item">Z</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Alphabet",
+    mounted() {
+      this.height = document.documentElement.clientHeight;
+      console.log(document.documentElement.clientHeight)
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import '~@styles/variables.styl';
+  .alphabet-list
+    position: fixed
+    top: 1.58rem
+    bottom: 0
+    right: 0rem
+    width: .4rem
+    color: $blueColor
+    display: flex
+    flex-direction: column
+    justify-content center
+    .al-item
+      margin .06rem 0
+      font-size .2rem
+</style>
